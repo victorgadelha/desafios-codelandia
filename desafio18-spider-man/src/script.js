1dc2e2b6aa1a888f8cf70db59d606bf81e427fc5
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2022-08-27 23:01:38 -0300

    feat: ajustando botao de saida do trailer

--- desafio18-spider-man/src/script.js
+++ desafio18-spider-man/src/script.js
@@ -1,6 +1,12 @@
 const navMenu = document.querySelector('.nav-menu__hamburguer-container')
 const navList = document.querySelector('.nav-menu__nav-list-container')
 const navLink = document.querySelectorAll('.nav-menu__nav-list')
+const button = document.querySelector('.trailer__button')
+const exitButton = document.querySelector('.modal-container__exit')
+const modal = document.querySelector('.modal-container')
+const trailer = document.querySelector('video')
+
+
 
 navMenu.addEventListener('click', () => {
     navMenu.classList.toggle('active')
@@ -10,3 +16,14 @@ navMenu.addEventListener('click', () => {
     });
 })
 
+button.addEventListener('click', () => {
+    modal.classList.toggle('hidden')
+    exitButton.classList.toggle('hidden')
+})
+
+exitButton.addEventListener('click', () => {
+    modal.classList.toggle('hidden')
+    trailer.currentTime = 0;
+    trailer.pause()
+    
+})
